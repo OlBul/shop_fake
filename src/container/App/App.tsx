7f16f09588e47fa7899b0671765cb2b9c629d569
OlBul <olhabulhakova@gmail.com>
2022-08-18 16:13:39 +0300

    Add header

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from 'container/Header/Header'
 
-type Props = {
-  title: string
-}
-
-const AppList = () => {
-  return (
-    <ul>
-      <li>List item 1</li>
-      <li>List item 2</li>
-      <li>List item 3</li>
-    </ul>
-  )
-}
-
-const AppHeader = (props: Props) => {
-  return <h1>Hello {props.title}</h1>
-}
 
 const App = () => {
   return (
     <>
-    <AppHeader title='App.js'/>
-    <AppHeader title='React js'/>
-    <AppList/>
+    <CssBaseline />
+    <Header />    
     </>
   )
 }

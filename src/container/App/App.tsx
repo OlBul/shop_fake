325f4fa913aa178a4263c7bb93133360c3705218
OlBul <olhabulhakova@gmail.com>
2022-08-20 15:18:46 +0300

    add ProductList and ProductListItem

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
     <CssBaseline />
     <Header />    
+    <Main/>    
     </>
   )
 }

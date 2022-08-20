0da149b911bad05dc3d76de2f4f861b640dc4211
OlBul <olhabulhakova@gmail.com>
2022-08-20 21:20:17 +0300

    add style

--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -19,13 +19,15 @@ const ProductList = (props: Props) => {
       spacing={3}
       >
         <Grid item xs={12} sm={6} md={4}>
-        <ProductListItem/> 
+        <ProductListItem name="iPhone X" description='This is iPhone X' type='phone' 
+        capacity={64} price={500}/> 
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
-        <ProductListItem/> 
+        <ProductListItem name="iPhone XS" description='This is iPhone XS' type='phone' capacity={64} price={400}/> 
         </Grid>
         <Grid item xs={12} sm={6} md={4}>
-        <ProductListItem/> 
+        <ProductListItem name="iPhone 13 Pro" description='This is iPhone 13 Pro' type='phone' 
+        capacity={128} price={1000}/> 
         </Grid>
         </Grid>   
       
--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,17 +1,31 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
+import './ProductListItem.scss'
 
-type Props ={}
+type ProductProps ={
+  name:string
+  description:string
+  type:string
+  capacity:number
+  price:number
 
-const ProductListItem = (props:Props) => {
+}
+
+const ProductListItem = ({
+  name, 
+  description, 
+  type,
+  capacity, 
+  price
+}:ProductProps) => {
   return (
     <Card>
       <CardContent>
-      <h4 className='product-title'>iPhone X</h4>
-      <div className='product-description'>This is iPhone X</div>
-      <div className='product-features'>Type: phone</div>
-      <div className='product-features'>Capacity: 64Gb</div>
-      <div className='product-price'>$ 500</div>
+      <h4 className='product-title'>{name}</h4>
+      <div className='product-description'>{description}</div>
+      <div className='product-features red'>Type: {type}</div>
+      <div className='product-features'>Capacity: {capacity}Gb</div>
+      <div className='product-price'>$ {price}</div>
       </CardContent>     
       <CardActions>
         <Button variant='contained'> Add to card  </Button>

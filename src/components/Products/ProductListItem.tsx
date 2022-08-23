05b99ec437b020790a62cd7eeb20458e7ddea8cb
OlBul <olhabulhakova@gmail.com>
2022-08-23 09:36:46 +0300

    add types for module

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -2,42 +2,41 @@ import { Button, Card, CardActions, CardContent } from '@mui/material'
 import React from 'react'
 import './ProductListItem.scss'
 
-export type ProductProps ={
-  image:string
-  id?:number
-  name:string
-  description:string
-  type:string
-  capacity:number
-  price:number
-
+export type ProductProps = {
+    image: string
+    id?: number
+    name: string
+    description: string
+    type: string
+    capacity: number
+    price: number
 }
 
 const ProductListItem = ({
-  image,
-  name, 
-  description, 
-  type,
-  capacity, 
-  price
-}:ProductProps) => {
-  return (
-    <Card>
-      <CardContent>
-        <div className='product-image'>
-          <img src={image} alt=''></img>
-        </div>
-      <h3 className='product-title'>{name}</h3>
-      <div className='product-description'>{description}</div>
-      <div className='product-features'>Type: {type}</div>
-      <div className='product-features'>Capacity: {capacity}Gb</div>
-      <div className='product-price'>$ {price}</div>
-      </CardContent>     
-      <CardActions className='btn-wrap'>
-        <Button variant='outlined'> Add to card  </Button>
-      </CardActions>
-    </Card>
-  )
+    image,
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}: ProductProps) => {
+    return (
+        <Card>
+            <CardContent>
+                <div className="product-image">
+                    <img src={image} alt=""></img>
+                </div>
+                <h3 className="product-title">{name}</h3>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}Gb</div>
+                <div className="product-price">$ {price}</div>
+            </CardContent>
+            <CardActions className="btn-wrap">
+                <Button variant="outlined"> Add to card </Button>
+            </CardActions>
+        </Card>
+    )
 }
 
-export default ProductListItem 
+export default ProductListItem

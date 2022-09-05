f55f40914c2b7d47a7a26bbdd9426529eebb999c
OlBul <olhabulhakova@gmail.com>
2022-09-05 22:39:08 +0300

    add getProductsObject

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import { keys } from 'lodash'
-import productsArray from 'components/Products/productsArray'
+import productsArray, {
+    getProductsObject,
+    Product,
+} from 'components/Products/productsArray'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
+    ProductsObject?: {
+        [id: number]: Product
+    }
 }
 
-const CartHeader = ({ productsInCart }: Props) => {
+const CartHeader = ({
+    productsInCart,
+    ProductsObject = getProductsObject(productsArray),
+}: Props) => {
     return (
         <div>
             {keys(productsInCart).map((productId) => (
                 <div key={productId}>
-                    {productsArray[parseInt(productId) - 1].name}:{' '}
+                    {ProductsObject[parseInt(productId)].name}:{' '}
                     {productsInCart[parseInt(productId)]}
                 </div>
             ))}
